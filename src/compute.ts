4a74fbf9703dd207599a6db50174a7a8860bcf74
GarikD <garikd@outlook.fr>
2022-04-25 13:27:32 +0200

    Update : Course model

--- src/compute.ts
+++ src/compute.ts
@@ -3,32 +3,32 @@ const courseList: course[] = [];
 
 const introAlgo: course = {
   grade: 11,
-  coef: 3.5,
+  weight: 3.5,
 };
 
 const reseaux: course = {
   grade: 11,
-  coef: 3.5,
+  weight: 3.5,
 };
 
 const sgbd: course = {
   grade: 14.25,
-  coef: 3.5,
+  weight: 3.5,
 };
 
 const modObj: course = {
   grade: 14.75,
-  coef: 3.5,
+  weight: 3.5,
 };
 
 const algoGraph: course = {
   grade: 16,
-  coef: 4,
+  weight: 4,
 };
 
 const gestRh: course = {
   grade: 14,
-  coef: 2,
+  weight: 2,
 };
 
 courseList.push(introAlgo, reseaux, sgbd, modObj, algoGraph, gestRh);
--- src/course.model.ts
+++ src/course.model.ts
@@ -1,4 +1,4 @@
 export interface course {
   grade: number;
-  coef: number;
+  weight: number;
 }
